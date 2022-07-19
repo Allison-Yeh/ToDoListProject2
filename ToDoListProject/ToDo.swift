13056a958ef35d75215a134e4a93bf6ea8badda2
Allison Yeh <allison.yeh@gmail.com>
2022-07-19 12:04:51 -0700

    Initial Commit

---
+++ ToDoListProject/ToDo.swift
@@ -0,0 +1,22 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by A Yeh on 7/18/22.
+//
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+    
+    init() {
+        
+    }
+}
+
+
+//var userName = ToDo()
+//userName.name = "Dishes"
+//userName.important = true
+
+
